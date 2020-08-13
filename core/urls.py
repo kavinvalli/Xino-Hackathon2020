5891c2ecd565275cc15b9b2c53708fa48f7ad14a
Kavin Desi Valli <kavinvalli@gmail.com>
2020-08-14 01:21:01 +0530

    Backend finish

--- core/urls.py
+++ core/urls.py
@@ -11,5 +11,14 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('travel-dashboard/', views.traveller_dashboard, name='travel-dashboard'),
     path('guide-detail/<int:guide_id>/', views.guide_detail, name='guide-detail'),
-    path('traveller-detail/<int:traveller_id>', views.traveller_detail, name='traveller-detail'),
+    path('traveller-detail/<int:traveller_id>/<int:hiring_id>', views.traveller_detail, name='traveller-detail'),
+    path('traveller-detail/<int:traveller_id>/<int:hiring_id>/yes', views.guide_interested, name='guide-interested'),
+    path('traveller-detail/<int:traveller_id>/<int:hiring_id>/no', views.guide_cancelled, name='guide-cancelled'),
+    path('traveller-detail/<int:traveller_id>/<int:hiring_id>/confirmed', views.guide_confirmed, name='guide-confirmed'),
+    path('traveller-detail/<int:traveller_id>/<int:hiring_id>/started', views.guide_started, name='guide-started'),
+    path('traveller-detail/<int:traveller_id>/<int:hiring_id>/ending', views.guide_ending, name='guide-ending'),
+    path("activate/<uidb64>/<token>/<int:traveller_id>/<int:guide_id>/<int:hiring_id>", views.start_meeting_link, name="startmeeting"),
+    path("end-activate/<uidb64>/<token>/<int:traveller_id>/<int:guide_id>/<int:hiring_id>", views.end_meeting_link, name="endmeeting"),
+    path('traveller-detail/<int:traveller_id>/<int:hiring_id>/paid', views.guide_paid, name='guide-paid'),
+    # def start_meeting_link(request, uidb64, token, traveller_id, guide_id)
 ]
